8bd3da461b4700d6a89de78ed85c63ab9d1aeb7c
Bhawandeep Singh <bhawandeep1998@gmail.com>
2025-05-13 21:53:24 +0530

    Initial commit from Create Next App

---
+++ app/Contact/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const contact = () => {
+  return (
+    <div>
+         <h1 className='text-2xl font-bold'>This Is My  Contact Page</h1>
+    </div>
+  )
+}
+
+export default contact
---
+++ app/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+    <h1 className='text-2xl font-bold'>This Is My Page</h1>
+    <a href='/Contact'>contact</a>
+    </>
+  )
+}
+
+export default page
