a51d94ba5cb5379c5c6bd5e46ebbd1207040219e
Bhawandeep Singh <bhawandeep1998@gmail.com>
2025-08-06 11:31:56 +0530

    first commit

--- app/Contact/page.js
+++ app/Contact/page.js
@@ -3,7 +3,8 @@ import React from 'react'
 const contact = () => {
   return (
     <div>
-         <h1 className='text-2xl font-bold'>This Is My  Contact Page</h1>
+         <h1 className='text-2xl px-5 py-5 font-bold flex justify-center'>Welcome </h1>
+         <h2 className='text-2xl px-5 py-5 font-bold  flex justify-center'>This Is My  Contact Page</h2>
     </div>
   )
 }
--- app/page.js
+++ app/page.js
@@ -3,8 +3,8 @@ import React from 'react'
 const page = () => {
   return (
     <>
-    <h1 className='text-2xl font-bold'>This Is My Page</h1>
-    <a href='/Contact'>contact</a>
+    <h1 className='text-2xl flex justify-center px-5 py-5 font-bold'>This Is My Page</h1>
+    <a href='/Contact' className='px-5 py-5 text-xl flex justify-center'>contact Click me </a>
     </>
   )
 }
